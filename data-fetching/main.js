b4550d904c5d256b011c7be98d23c861450b2371
Valeria Vassallo <valevassallo1@gmail.com>
2019-05-21 17:31:25 -0500

    Check storage

--- data-fetching/main.js
+++ data-fetching/main.js
@@ -1,26 +1,34 @@
 const URL = "https://ghibliapi.herokuapp.com/films";
 const filmsList = document.getElementById("films");
+
 async function getData() {
   await fetch(URL)
     .then(response => response.json())
     .then(films => {
-      return films.map(film => {
-        showDom(film);
-        localStorage.setItem("storedFilms", JSON.stringify(films));
-      });
+      showDom(films);
+      localStorage.setItem("storedFilms", JSON.stringify(films));
     });
 }
 
 function showDom(element) {
-  let li = document.createElement("li"),
-    h2 = document.createElement("h2"),
-    p = document.createElement("p");
+  return element.map(film => {
+    let li = document.createElement("li"),
+      h2 = document.createElement("h2"),
+      p = document.createElement("p");
 
-  h2.innerHTML = `${element.title}`;
-  p.innerHTML = `${element.description}`;
-  li.appendChild(h2);
-  li.appendChild(p);
-  filmsList.appendChild(li);
+    h2.innerHTML = `${film.title}`;
+    p.innerHTML = `${film.description}`;
+    li.appendChild(h2);
+    li.appendChild(p);
+    filmsList.appendChild(li);
+  });
+}
+
+const storage = localStorage.getItem("storedFilms");
+
+if (storage != null) {
+  let parsedFilms = JSON.parse(storage);
+  showDom(parsedFilms);
 }
 
 getData();
